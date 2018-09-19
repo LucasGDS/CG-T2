2977bbf57c3b52ce383174241b109b17a94256bb
lucas <lucas@teia.telemidia.puc-rio.br>
2018-09-19 14:13:07 -0300

    add draw lines

--- public/javascripts/bezier.js
+++ public/javascripts/bezier.js
@@ -5,17 +5,31 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 //ctx.font = '48px serif';
 //ctx.fillText("escolha a imagem",50,100);
 
+var listapontos = [];
+var next = 0;
+
 function writeMessage(canvas, message) {
-  var context = canvas.getContext('2d');
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  context.font = '11pt Calibri';
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.font = '11pt Calibri';
   ctx.fillStyle = 'white';
   ctx.fillText(message, 10, 25);
+
+  for (let i=0;i<next-1;++i)
+  {
+    ctx.beginPath();
+    ctx.moveTo(listapontos[i].x,listapontos[i].y);
+    ctx.lineTo(listapontos[i+1].x,listapontos[i+1].y);
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+  }
 }
 function getMousePos(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
@@ -28,4 +42,19 @@ canvas.addEventListener('mousemove', function(evt) {
   var mousePos = getMousePos(canvas, evt);
   var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
   writeMessage(canvas, message);
+
+  ctx.beginPath();
+  ctx.moveTo(listapontos[next-1].x,listapontos[next-1].y);
+  ctx.lineTo(mousePos.x,mousePos.y);
+  ctx.strokeStyle = 'red';
+  ctx.stroke();
+
+}, false);
+
+canvas.addEventListener('click', function(evt) {
+  var mousePos = getMousePos(canvas, evt);
+  listapontos[next] = {x : mousePos.x, y: mousePos.y};
+  console.log(listapontos);
+  ++next;
+
 }, false);
