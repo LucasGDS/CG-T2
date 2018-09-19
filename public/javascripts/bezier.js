70674e83ecbba1213ab65958169980c86e80abd0
lucas <lucas@teia.telemidia.puc-rio.br>
2018-09-19 15:03:35 -0300

    add draw 4 points

--- public/javascripts/bezier.js
+++ public/javascripts/bezier.js
@@ -13,6 +13,7 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 var listapontos = [];
 var next = 0;
+var drawing = 0;
 
 function writeMessage(canvas, message) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -22,14 +23,22 @@ function writeMessage(canvas, message) {
   ctx.fillStyle = 'white';
   ctx.fillText(message, 10, 25);
 
-  for (let i=0;i<next-1;++i)
+  for (let i=0;i<next;++i)
   {
+    ctx.beginPath();
+    ctx.fillStyle = 'blue'; 
+    ctx.arc(listapontos[i].x, listapontos[i].y, 5, 0, 2 * Math.PI, true); 
+    ctx.fill(); 
+
+    if(i == next-1)
+      break;
     ctx.beginPath();
     ctx.moveTo(listapontos[i].x,listapontos[i].y);
     ctx.lineTo(listapontos[i+1].x,listapontos[i+1].y);
     ctx.strokeStyle = 'red';
     ctx.stroke();
   }
+  
 }
 function getMousePos(canvas, evt) {
   var rect = canvas.getBoundingClientRect();
@@ -43,18 +52,43 @@ canvas.addEventListener('mousemove', function(evt) {
   var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
   writeMessage(canvas, message);
 
-  ctx.beginPath();
-  ctx.moveTo(listapontos[next-1].x,listapontos[next-1].y);
-  ctx.lineTo(mousePos.x,mousePos.y);
-  ctx.strokeStyle = 'red';
-  ctx.stroke();
+  if (drawing === 1)
+  {
+    ctx.beginPath();
+    ctx.moveTo(listapontos[next-1].x,listapontos[next-1].y);
+    ctx.lineTo(mousePos.x,mousePos.y);
+    ctx.strokeStyle = 'red';
+    ctx.stroke(); 
 
+    ctx.beginPath();
+    ctx.fillStyle = 'blue'; 
+    ctx.arc(mousePos.x, mousePos.y, 5, 0, 2 * Math.PI, true); 
+    ctx.fill(); 
+  }
+  else if (drawing === 2)
+  {
+    
+  } 
 }, false);
 
 canvas.addEventListener('click', function(evt) {
   var mousePos = getMousePos(canvas, evt);
-  listapontos[next] = {x : mousePos.x, y: mousePos.y};
+
+  if(drawing === 0 || drawing === 1)
+  {
+    listapontos[next] = {x : mousePos.x, y: mousePos.y};
+    ++next;
+  }
   console.log(listapontos);
-  ++next;
+
+  if(next === 1)
+  {
+    drawing = 1;
+  }
+
+  if(next === 4)
+  {
+    drawing = 2;
+  }
 
 }, false);
